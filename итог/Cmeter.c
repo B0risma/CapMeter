1d0df1bbd4cf1415dec6c4ce610199890b5b0dd2
B0risma <borisma1999@yandex.ru>
2021-10-02 01:51:21 +0500

    Update Cmeter.c
    disable pull-up for adc measuring

--- итог/Cmeter.c
+++ итог/Cmeter.c
@@ -1,29 +1,30 @@
-#define CUR2 PORTD.7
-#define CUR1 PORTB.0
-#define DISCH PORTB.1
-#define TESTC PORTD.0
 #define LCD_LINE1 lcd_com(0x80)
-#define LCD_LINE2 lcd_com(0xC0)
-#define LCD_CLR lcd_com(0x01)
-#define CLK 16  //MHz
-#define CLK_PSL 8
-#define REF 1500
+#define LCD_LINE2 lcd_com(0xC0) //Макрокоманды для
+#define LCD_CLR lcd_com(0x01)   //работы дисплея
+
+#define CLK 25  //Макроопределение тактовой частоты
+#define CLK_PSL 8  //Макроопределение делителя тактовой частоты таймера
+
+//Макроопределения для работы с ножками контроллера
+#define CUR2 PORTD.7    //Ножка связанная с источником тока 2
+#define CUR1 PORTB.0    //Ножка связанная с источником тока 2
+#define DISCH PORTB.1   //Связанная с разрядным ключом
+#define TESTC PORTD.0   //Связанная с подключением тестового конденсатора
+
+#define REF 1235        //Макроопределение опорного напряжения АЦП
 
 
 #include <mega8.h>
-
-// Standard Input/Output functions
-//#include <stdio.h>
 #include <stdlib.h>
-//#include <string.h>
 #include <iobits.h>
 #include <delay.h>
-#include <spi.h>
+#include <spi.h>        
+//#include <lcd_spi.h>    //Функции для работы с дисплеем
 
 // Declare your global variables here
-unsigned long t=0, T1=0, T2=0;
-eeprom float I1=10.5, I2= 113.3, prb_R = 1;
-bit mode = 0;
+unsigned long t=0, T1=0, T2=0;              //Переменные времени
+eeprom float I1=10, I2= 100, prb_R = 0;     //Переменные калибровочных значений
+bit mode = 0;                               //Флаг режима работы
 
 void LCDcom(unsigned char com) //выполняет пол команды отправляет старший полубайт
 {
@@ -139,15 +140,20 @@ void lcd_printf(float f, unsigned char n){
     str_out(str);
 }
 
-float probe_R (void)
+
+//Измерение сопротивления щупов
+float probe_R (void)        
 {
       char i=0; 
       float prb_r = 0;
+      int adc = 0;
       DISCH = 0;
       CUR2 = 1;
       LCD_CLR;
       LCD_LINE1;
-      strf_out("connect ");
+      strf_out("Connect");
+      LCD_LINE2;
+      strf_out("probes");
       delay_ms(100);
       #asm("cli")
       ACSR.ACD = 1;
@@ -157,31 +163,46 @@ float probe_R (void)
       DDRC.3 = 0;
       PORTC.3 = 1;
       
+      
       while(TSTBIT(PINC, 3));
-      delay_ms(100);
+      PORTC.3 = 0;
+      delay_ms(1000);
       ADCSRA.ADEN = 1;
-      for ( i = 0; i < 10; i++)
+      for ( i = 0; i < 100;)
       {
           ADCSRA.ADSC = 1;
           while(!TSTBIT(ADCSRA, ADIF));
           SETBIT(ADCSRA, ADIF);
-          prb_r += ADCW;
+          
+          if (ADCW > adc/2){
+            adc = ADCW;
+            prb_r += adc;
+            i++;
+          }
+          
+          
       }
-      prb_r = (prb_r*REF/1024)/(I2*10);
+      prb_r = (prb_r*REF/1024)/(I2*100);
       LCD_CLR;
       LCD_LINE1;
-      strf_out("disc ");
+       lcd_printf(ADCW,0);
+      strf_out("Dis");
+      LCD_LINE2;
+      strf_out(" connect");
       ADCSRA.ADEN = 0;
       while(!TSTBIT(PINC, 3));
       ACSR.ACD = 0;
       CUR2 = 0;
       LCD_CLR;
-      LCD_LINE1;     
+      LCD_LINE1;
+      ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0));     
       
       return prb_r;
 }
 
-void check( float C){
+
+//Калибровка источников тока
+void check( float C){          
  CUR1 = 0;
  CUR2 = 0;
  mode = 1;  
@@ -191,7 +212,7 @@ void check( float C){
  SETBIT(SFIOR, ACME);
  SETBIT(TCCR1B,CS11);
  #asm("sei")
- //TESTC = 1;
+ TESTC = 1;
  DISCH = 1;
  delay_ms(1000);  
  CUR2=1;
@@ -207,9 +228,6 @@ void check( float C){
  while(!T2);
  CUR2=0;
  I2 = (1265)/((float)T2/(CLK/CLK_PSL))*C;
- 
-
- 
  
  DISCH=1;
  delay_ms(1000);
@@ -228,8 +246,7 @@ void check( float C){
  CUR1=0;
  I1 = (1265)/((float)T2/(CLK/CLK_PSL))*C;
  
- 
- DISCH=1;
+ DISCH=0;
  LCD_CLR;
  LCD_LINE1; 
  lcd_printf(I1,1 );
@@ -237,24 +254,23 @@ void check( float C){
  LCD_LINE2;
  lcd_printf(I2,1); 
  strf_out("mA");
- //TESTC = 0;
+ TESTC = 0;
  
- /*delay_ms(500);
+ delay_ms(500);
+ prb_R = probe_R();
  LCD_CLR;
- strf_out("probe R=");
- LCD_LINE2;
- prb_R = probe_R(); 
+ strf_out("Probe R=");
+ LCD_LINE2; 
  lcd_printf(prb_R,3);
- delay_ms(500);*/ 
- 
+ delay_ms(500); 
                 
  mode = 0;
  CLRBIT(TCCR1B,CS11); 
-        
-
 }
 
-float testC(){
+
+//Измерение ёмкости конденсатора
+float testC(){              
 float C = 0;
 #asm("cli")
 CUR1 = 0;
@@ -262,7 +278,7 @@ CUR2 = 0;
 DISCH=1;
 delay_ms(1000);
 SETBIT(TCCR1B,CS11);
-ADMUX = 0;
+ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0));
 SETBIT(TIFR,ICF1);
 #asm("sei")
 CUR1=1;
@@ -273,12 +289,12 @@ t = 0;
 T2 = 0;
 DISCH = 0;
 while (!T1);
-if (T1/0xffff > (CLK/CLK_PSL)*1) {  //~>1000uF
+if (T1/0xffff > (CLK/CLK_PSL)*1) {  // Переключение источников тока, если С > 1000uF
     #asm("cli")
     CUR1 = 0;
     DISCH = 1;
     delay_ms(1000);
-    ADMUX = 0;
+    ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0));
     SETBIT(TIFR,ICF1);
     #asm("sei")
     CUR2 = 1;
@@ -301,29 +317,29 @@ else {
 return C;
 }
 
-
-float esr(float C){
+//Измерение ESR
+float esr(float C){             
     float R = 0;
+    
     #asm("cli")
     CUR1 = 0;
-    CUR2 = 0;
+    CUR2 = 1;
     DISCH = 1;
-    delay_ms(2000);
+    delay_ms(1000);
     SETBIT(TCCR1B,CS11);
     t=0;
     T1 = 0; 
-    //CLRBIT(ADMUX,MUX0);
-    ADMUX = (1 << MUX1) | (0 << MUX0);
+    ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0));
+    ADMUX |= (0 << MUX1) | (0 << MUX0);
     SETBIT(TIFR,ICF1);
     TCNT1 = 0;
-    //SETBIT(TCCR1B,CS11); 
     SETBIT(ACSR,ACI);
     #asm("sei")
+    
     DISCH = 0;
-    CUR2 = 1;
     TCNT1 = 0;    
     while(!T1 );
-    R = (1235)/I2 - (T1/C)/(CLK/CLK_PSL);// - prb_R;          
+    R = (1235)/I2 - (T1/C)/(CLK/CLK_PSL);// - prb_R;         
     CUR1 = 0;
     CUR2 = 0;
     DISCH = 1;
@@ -333,7 +349,7 @@ float esr(float C){
 
 
 
-// External Interrupt 0 service routine
+//Обработка кнопки запуска калибровки
 interrupt [EXT_INT0] void ext_int0_isr(void)
 {
     delay_ms(100);
@@ -344,7 +360,7 @@ interrupt [EXT_INT0] void ext_int0_isr(void)
 
 
 
-// Timer 0 overflow interrupt service routine
+//Прерывание по переполнению таймера
 interrupt [TIM1_OVF] void timer1_ovf_isr(void)
 {
         t++;
@@ -355,17 +371,17 @@ interrupt [TIM1_OVF] void timer1_ovf_isr(void)
 
 }
 
-
+//Прерывание через блок захвата по сработке компаратора
 interrupt [TIM1_CAPT] void timer1_capt_isr(void)
 {
         
         if (TSTBIT(ADMUX, MUX1)) {
             TCNT1 = 0;
-            ADMUX = 0;
+            ADMUX &= ~((1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0));
             }
         else if (!TSTBIT(ADMUX, MUX0)){
             TCNT1 = 0;
-            SETBIT(ADMUX, MUX0);
+            ADMUX |= (1<<MUX0);
             T1 = t*0xFFFF + ICR1;  
             //T2=0;
             t=0;
@@ -388,44 +404,29 @@ void main(void)
 float C = 0, R=0;
 
 
-// Input/Output Ports initialization
-// Port B initialization
-// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+//Инициализация необходимых блоков при запуске
+// Настройка портов ввода-вывода
+// Port B
 DDRB=(0<<DDB7) | (0<<DDB6) | (1<<DDB5) | (0<<DDB4) | (1<<DDB3) | (1<<DDB2) | (1<<DDB1) | (1<<DDB0);
-// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
 PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (0<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
 
-// Port C initialization
-// Function: Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+// Port C 
 DDRC=(0<<DDC6) | (0<<DDC5) | (0<<DDC4) | (0<<DDC3) | (0<<DDC2) | (0<<DDC1) | (0<<DDC0);
-// State: Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
 PORTC=(0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
 
-// Port D initialization
-// Function: Bit7=In Bit6=In Bit5=In Bit4=In Bit3=In Bit2=In Bit1=In Bit0=In 
+// Port D
 DDRD=(1<<DDD7) | (0<<DDD6) | (0<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (1<<DDD0);
-// State: Bit7=T Bit6=T Bit5=T Bit4=T Bit3=T Bit2=T Bit1=T Bit0=T 
 PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (0<<PORTD4) | (0<<PORTD3) | (1<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
 
-// Timer/Counter 0 initialization
-// Clock source: System Clock
-// Clock value: 1000,000 kHz
-TCCR0=(0<<CS02) | (0<<CS01) | (0<<CS00);
-TCNT0=0x00;
 
-// Timer/Counter 1 initialization
-// Clock source: System Clock
-// Clock value: 1000,000 kHz
-// Mode: Normal top=0xFFFF
-// OC1A output: Disconnected
-// OC1B output: Disconnected
-// Noise Canceler: Off
-// Input Capture on Rising Edge
-// Timer Period: 65,536 ms
-// Timer1 Overflow Interrupt: On
-// Input Capture Interrupt: On
-// Compare A Match Interrupt: Off
-// Compare B Match Interrupt: Off
+// Ииициализация таймера-счтечика 1
+// Источник тактов: тактирование МК
+// Тактовая частота: CLK/CLK_PSL = 2 kHz
+// Фильтр помех: On
+// Источник сигнала захвата: выход компаратора
+// Захват таймера: по нарастающему фронту
+// Прерывание по переполнению: On
+// Прерывание по захвату: On
 TCCR1A=(0<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (0<<WGM11) | (0<<WGM10);
 TCCR1B=(1<<ICNC1) | (1<<ICES1) | (0<<WGM13) | (0<<WGM12) | (0<<CS12) | (0<<CS11) | (0<<CS10);
 TCNT1H=0x00;
@@ -437,28 +438,19 @@ OCR1AL=0x00;
 OCR1BH=0x00;
 OCR1BL=0x00;
 
-// Timer/Counter 2 initialization
-// Clock source: System Clock
-// Clock value: Timer2 Stopped
-// Mode: Normal top=0xFF
-// OC2 output: Disconnected
-ASSR=0<<AS2;
-TCCR2=(0<<PWM2) | (0<<COM21) | (0<<COM20) | (0<<CTC2) | (0<<CS22) | (0<<CS21) | (0<<CS20);
-TCNT2=0x00;
-OCR2=0x00;
 
-// Timer(s)/Counter(s) Interrupt(s) initialization
+// Настройка прерывания таймера 1
 TIMSK=(0<<OCIE2) | (0<<TOIE2) | (1<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (1<<TOIE1) | (0<<TOIE0);
 
-// External Interrupt(s) initialization
-// INT0: On
-// INT0 Mode: Any change
+// Внешние прерывания
+// По ножке INT0: On
+// Режим по INT0: любое изменение
 // INT1: Off
 GICR|=(0<<INT1) | (1<<INT0);
 MCUCR=(0<<ISC11) | (0<<ISC10) | (0<<ISC01) | (1<<ISC00);
 GIFR=(0<<INTF1) | (1<<INTF0);
 
-// USART initialization
+/*// USART initialization
 // Communication Parameters: 8 Data, 1 Stop, No Parity
 // USART Receiver: Off
 // USART Transmitter: On
@@ -468,39 +460,27 @@ UCSRA=(0<<RXC) | (0<<TXC) | (0<<UDRE) | (0<<FE) | (0<<DOR) | (0<<UPE) | (0<<U2X)
 UCSRB=(0<<RXCIE) | (0<<TXCIE) | (0<<UDRIE) | (0<<RXEN) | (0<<TXEN) | (0<<UCSZ2) | (0<<RXB8) | (0<<TXB8);
 UCSRC=(1<<URSEL) | (0<<UMSEL) | (0<<UPM1) | (0<<UPM0) | (0<<USBS) | (1<<UCSZ1) | (1<<UCSZ0) | (0<<UCPOL);
 UBRRH=0x00;
-UBRRL=0x33;
+UBRRL=0x33;*/
 
-// Analog Comparator initialization
-// Analog Comparator: On
-// The Analog Comparator's positive input is
-// connected to the AIN0 pin
-// The Analog Comparator's negative input is
-// connected to the ADC multiplexer
-// Interrupt on Rising Output Edge
-// Analog Comparator Input Capture by Timer/Counter 1: Off
+// Аналоговый копаратор
+// Неинвертирующий вход: AIN0
+// Инвертирующий вход: мультиплексор АЦП
 ACSR=(0<<ACD) | (0<<ACBG) | (0<<ACO) | (1<<ACI) | (0<<ACIE) | (1<<ACIC) | (1<<ACIS1) | (1<<ACIS0);
 SFIOR=(1<<ACME);
 
-// ADC initialization
-// ADC disabled
+// AЦП
+// Источник опорного напряжения: внешнее
 ADCSRA=(0<<ADEN) | (0<<ADSC) | (0<<ADFR) | (0<<ADIF) | (0<<ADIE) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
-ADMUX = 0;
+ADMUX = (0<< REFS1) | (0<<REFS0)| (0<<ADLAR);
 
-// SPI initialization
-// SPI Type: Master
-// SPI Clock Rate: 500,000 kHz
-// SPI Clock Phase: Cycle Start
-// SPI Clock Polarity: Low
-// SPI Data Order: MSB First
+// SPI
+// Режим: Master
 if (CLK <= 16) SPCR=(0<<SPIE) | (1<<SPE) | (0<<DORD) | (1<<MSTR) | (0<<CPOL) | (0<<CPHA) | (0<<SPR1) | (1<<SPR0);
 else   SPCR=(0<<SPIE) | (1<<SPE) | (0<<DORD) | (1<<MSTR) | (0<<CPOL) | (0<<CPHA) | (1<<SPR1) | (1<<SPR0);
 SPSR=(0<<SPI2X);
 
-// TWI initialization
-// TWI disabled
-TWCR=(0<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWEN) | (0<<TWIE);
 
-// Global enable interrupts
+//Начало работы алгоритма измерений
 DISCH=1;
 SETBIT(TIFR,ICF1);
 #asm("cli")
@@ -513,9 +493,6 @@ lcd_printf(I2,1);
 strf_out("mA");
 delay_ms(500);
 
-
-
-
 LCD_CLR;
 strf_out("TESTING");
 
@@ -526,7 +503,7 @@ R = esr(C);
 CLRBIT(TCCR1B,CS11);
 
       
-
+// Вывод результатов
 while (1)
       { 
         LCD_CLR;
@@ -540,7 +517,7 @@ while (1)
         strf_out("    uf");
         delay_ms(1000);
         LCD_CLR;
-        lcd_printf(R, 3); //!!!!!!!!!!!!!
+        lcd_printf(R, 2);
         LCD_LINE2;
         strf_out("    ohm");
         delay_ms(1000);
